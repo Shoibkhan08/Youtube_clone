917cd598fd19cff80ba53d678783ab8466fc6afe
Shoib Khan <142472630+Shoibkhan08@users.noreply.github.com>
2024-01-05 01:24:49 +0530

    Update fetchFromAPI.js

--- src/component/API/fetchFromAPI.js
+++ src/component/API/fetchFromAPI.js
@@ -6,11 +6,8 @@ export default function fetchFromAPI(){
   
   const getData = async (url)=>{
     const BASE_URL = `https://youtube.googleapis.com/youtube/v3/`;
-    // const API_KEY = `AIzaSyCH2g1YbyTWb45ZrU_cLLtSrcVrO2pkQQo`;
-    // const API_KEY = `AIzaSyBkt9eteKkCb5jBSm6NnLnuSXd04KazbD0`;
-    const API_KEY = `AIzaSyA28o1Is7To03i2jcwFwXS2a-IQn2um2BA`;
-    // const API_KEY = `AIzaSyA0tif98M74ROqAEp20Okj-pqKnVnlaF9k`;
-    // const API_KEY = `AIzaSyAVv9DM2pSP88v4mBlrfpwxmMzxSKM3bW4`;
+    const API_KEY = `AIzaSyBkt9eteKkCb5jBSm6NnLnuSXd04KazbD0`;
+    
     
     try {
       // const response = await axios.get(`${BASE_URL}search?part=snippet&maxResults=50&q=surfing&key=${API_KEY}`)
